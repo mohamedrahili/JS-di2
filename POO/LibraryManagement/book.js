495ec8dc9e0e3b03c97cf0c0e92361dd54da2eef
mohamedrahili <mohamedrahili136@gmail.com>
2025-01-02 11:48:07 +0100

    Describe your changes here

--- POO/LibraryManagement/book.js
+++ POO/LibraryManagement/book.js
@@ -1,12 +1,29 @@
 class Book {
-    constructor(title, author, year, pages) {
+    constructor(title, author, year, pages, price) {
         this.title = title;
         this.author = author;
         this.year = year;
         this.pages = pages;
+        this._price = price; 
     }
+
+    // Getter for price
+    get price() {
+        return this._price;
+    }
+
+    // Setter methodes for price with validation
+    set price(newPrice) {
+        if (newPrice > 0) {
+            this._price = newPrice;
+        } else {
+            console.log("The price must be greater than 0.");
+        }
+    }
+
     showinfo() {
-        console.log(`The book named ${this.title} comme with ${this.pages} page was written by the author ${this.author} in ${this.year}`);
+        console.log(`The book named ${this.title} with ${this.pages} pages was written by the author ${this.author} in ${this.year}. The price is $${this.price}.`);
     }
 }
+
 module.exports = Book;
--- POO/LibraryManagement/library.js
+++ POO/LibraryManagement/library.js
@@ -1,15 +1,17 @@
 const Book = require('./book');
 
 class Library {
-    constructor(name = "Library", books = [], year = 0, pages = 0) {
+    constructor(name = "Library", books = []) {
         this.name = name;
         this.books = books;
-        this.year = year;
-        this.pages = pages;
     }
 
     addBook(book) {
-        this.books.push(book);
+        if (book.price > 0) {
+            this.books.push(book);
+        } else {
+            console.log(`Cannot add book "${book.title}" with invalid price: ${book.price}`);
+        }
     }
 
     removeBook(book) {
@@ -23,13 +25,17 @@ class Library {
     getBookByAuthor(author) {
         return this.books.filter((b) => b.author === author);
     }
-    
+
     getBooksByYear(year) {
         return this.books.filter((b) => b.year === year);
     }
 
     getBooksByPages(pages) {
         return this.books.filter((b) => b.pages === pages);
+    }
+
+    getBooksByPrice(price) {
+        return this.books.filter((b) => b.price === price);
     }
 
     listBooks() {
@@ -42,16 +48,16 @@ class Library {
 }
 
 // Book instances
-const book1 = new Book("Unlock It", "Dan Lok", 2019, 240);
-const book2 = new Book("F.U. Money", "Dan Lok", 2014, 210);
-const book3 = new Book("Influence", "Dan Lok", 2021, 180);
-const book4 = new Book("Think and Grow Rich", "Napoleon Hill", 1937, 238);
-const book5 = new Book("The 7 Habits of Highly Effective People", "Stephen R. Covey", 1989, 381);
-const book6 = new Book("How to Win Friends and Influence People", "Dale Carnegie", 1936, 291);
-const book7 = new Book("Rich Dad Poor Dad", "Robert T. Kiyosaki", 1997, 207);
-const book8 = new Book("The Lean Startup", "Eric Ries", 2011, 336);
-const book9 = new Book("Atomic Habits", "James Clear", 2018, 320);
-const book10 = new Book("The E-Myth Revisited", "Michael E. Gerber", 1995, 288);
+const book1 = new Book("Unlock It", "Dan Lok", 2019, 240, 100);
+const book2 = new Book("F.U. Money", "Dan Lok", 2014, 210, 80);
+const book3 = new Book("Influence", "Dan Lok", 2021, 180, 120);
+const book4 = new Book("Think and Grow Rich", "Napoleon Hill", 1937, 238, 90);
+const book5 = new Book("The 7 Habits of Highly Effective People", "Stephen R. Covey", 1989, 381, 150);
+const book6 = new Book("How to Win Friends and Influence People", "Dale Carnegie", 1936, 291, 110);
+const book7 = new Book("Rich Dad Poor Dad", "Robert T. Kiyosaki", 1997, 207, 70);
+const book8 = new Book("The Lean Startup", "Eric Ries", 2011, 336, 130);
+const book9 = new Book("Atomic Habits", "James Clear", 2018, 320, 100);
+const book10 = new Book("The E-Myth Revisited", "Michael E. Gerber", 1995, 288, -250); // Invalid price
 
 // Initialize Library
 const library = new Library();
@@ -67,7 +73,7 @@ library.addBook(book6);
 library.addBook(book7);
 library.addBook(book8);
 library.addBook(book9);
-library.addBook(book10);
+library.addBook(book10); 
 
 // List all books
 console.log("\nListing all books in the library:");
@@ -87,7 +93,7 @@ console.log("\nGetting books by author Dan Lok:");
 const booksByAuthor = library.getBookByAuthor("Dan Lok");
 booksByAuthor.forEach((book) => console.log(`${book.title} by ${book.author}`));
 
-// Get bokks by year
+// Get books by year
 console.log("\nGetting books by year 2014:");
 const booksByYear = library.getBooksByYear(2014);
 booksByYear.forEach((book) => console.log(`${book.title} by ${book.author}`));
@@ -96,6 +102,11 @@ booksByYear.forEach((book) => console.log(`${book.title} by ${book.author}`));
 console.log("\nGetting books by page 240:");
 const booksByPage = library.getBooksByPages(240);
 booksByPage.forEach((book) => console.log(`${book.title} by ${book.author}`));
+
+// Get books by price
+console.log("\nGetting books by price $10:");
+const booksByPrice = library.getBooksByPrice(10);
+booksByPrice.forEach((book) => console.log(`${book.title} by ${book.author}`));
 
 // Remove a book
 console.log("\nRemoving a book from the library...");
